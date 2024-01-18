9fa5fba329df711c2b54234fb78f11b215eab859
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-18 23:32:10 +0530

    colors added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Link from "next/link"
 import Links from'./links/Links'
-
+import styles from'./navbar.module.css'
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.container}>
     <div>Logo</div>
     <div>
     <Links/>
