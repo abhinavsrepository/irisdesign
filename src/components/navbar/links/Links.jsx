548c49633b4b8f411865f9b443d98c77a4126790
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-21 17:04:27 +0530

    navbar addition

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,6 @@
 import styles from './links.module.css'
 import Link from 'next/link';
+import NavLink from './navLink/navLink';
 
  const Links = () => {
     const links =[
@@ -15,23 +16,38 @@ import Link from 'next/link';
             title:"Contact",
             path:"/contact",
         },
-        {
-            title:"Homepage",
-            path:"/",
-        },
+        
         {
             title:"Blog",
             path:"/blog",
         },
-    ];
+    ]
+        const session = true;
+        const isAdmin =true;
+    
   return (
-    <div className={styles.links}>{
-        links.map((link=>(
-            <Link href={link.path} key={link.title}>{link.title}
-            </Link>
-        )))}
+    <div className={styles.links}>
+        {links.map((link)=>(
+            <NavLink item={link} Key={link.title}/>
+            // <Link href={link.path} key={link.title}>{link.title}
+           
+        ))}{
+            session ?(
+                <>
+                {
+                            isAdmin && 
+                                <NavLink item ={{title:"Admin",path:"/admin"}} />
+                            
+                        }
+                        <button>LogOut</button>
+                        </>
+
+                    ):(
+                <NavLink item={{title:"Login",path:"/login"}}/>
+            )
+        }
 
     </div>
   )
-}
+};
 export default Links
