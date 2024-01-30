c1f308cb87e56b3d219034cce225b10382bd4c60
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-30 23:38:44 +0530

    images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import styles from "./about.module.css"
 const AboutPage = () => {
   return (
     <div>
-      <Image src="/about.png" alt="" width={500} height={500}/>
+      <div className={StyleSheet.imgContainer}>
+      <Image src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill/>
+    </div>
     </div>
   );
 };
